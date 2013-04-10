e952e371cf8b8c7f30aa712c75c5403b10437f8a
Tobias Roeser <tobias.roeser@tototec.de>
2013-04-10 21:43:47 +0200

    Current version of buildfile.

--- SBuild.scala
+++ SBuild.scala
@@ -8,7 +8,7 @@ import java.io.File
 @classpath("mvn:org.apache.ant:ant:1.8.4")
 class SBuild(implicit _project: Project) {
 
-  Target("phony:all") dependsOn "compileScala-quick-compiler"
+  Target("phony:all") dependsOn "locker-all" ~ "quick-all"
 
   Target("phony:clean").evictCache exec {
     AntDelete(dir = Path("target"))
@@ -25,12 +25,12 @@ class SBuild(implicit _project: Project) {
   val ant = "mvn:org.apache.ant:ant:1.8.4"
   val jline = "lib/jline.jar"
 
-  val stagesToSkipIfDirExists: Seq[String] = Seq("locker")
+  val stagesToSkipIfDirExists: Seq[String] = Prop("skipStages", "").split(",").filter(s => s.trim.length > 0)
 
   // TODO: directly use publically available jars
-  val starrCompilerCp = 
-    "lib/scala-library.jar" ~ 
-    "lib/scala-reflect.jar" ~ 
+  val starrCompilerCp =
+    "lib/scala-library.jar" ~
+    "lib/scala-reflect.jar" ~
     "lib/scala-compiler.jar" ~
     forkjoinJar
 
@@ -58,9 +58,15 @@ class SBuild(implicit _project: Project) {
     AntJar(destFile = ctx.targetFile.get, baseDir = Path("target/forkjoin/classes"))
   }
 
+  def cleaner(producerName: String, dirToDelete: File): Target = {
+    Target("phony:clean-"+producerName) evictCache producerName exec {
+      AntDelete(dir = dirToDelete)
+    }
+  }
+
   // TODO: excludes
-  def stagedJavac(stage: String, project: String, 
-                  outputDir: Option[String] = None, 
+  def stagedJavac(stage: String, project: String,
+                  outputDir: Option[String] = None,
                   args: Seq[String] = Seq(),
                   classpath: TargetRefs = TargetRefs(),
                   dependsOn: TargetRefs = TargetRefs()): Target = {
@@ -69,10 +75,10 @@ class SBuild(implicit _project: Project) {
       case None => Path(s"target/${stage}/${project}/classes")
       case Some(dir) => Path(dir)
     }
-    val t = Target(s"phony:compileJava-${stage}-${project}").cacheable dependsOn 
+    val t = Target(s"phony:compileJava-${stage}-${project}").cacheable dependsOn
       dependsOn ~ classpath ~ sources exec { ctx: TargetContext =>
       if(stagesToSkipIfDirExists.contains(stage) && targetDir.exists) {
-        println("Skipping")
+        println(s"Skipping stage ${stage}")
       } else {
         addons.java.Javac(
           sources = sources.files,
@@ -84,7 +90,10 @@ class SBuild(implicit _project: Project) {
       }
       ctx.attachFile(targetDir)
     }
-    Target("phony:" + stage) dependsOn t
+
+    Target(s"phony:clean-${stage}") dependsOn cleaner(s"java-${stage}-${project}", targetDir)
+
+    Target(s"phony:${stage}-all") dependsOn t
     t
   }
 
@@ -112,7 +121,7 @@ class SBuild(implicit _project: Project) {
     val t = Target(s"phony:compileScala-${stage}-${project}").cacheable dependsOn 
       compilerClasspath ~ dependsOn ~ classpath ~ sources exec { ctx: TargetContext =>
       if(stagesToSkipIfDirExists.contains(stage) && targetDir.exists) {
-        println("Skipping")
+        println(s"Skipping stage ${stage}")
       } else {
         addons.scala.Scalac(
           sources = sources.files,
@@ -129,91 +138,70 @@ class SBuild(implicit _project: Project) {
       }
       ctx.attachFile(targetDir)
     }
-    Target("phony:" + stage) dependsOn t
+
+    Target(s"phony:clean-${stage}") dependsOn cleaner(s"scala-${stage}-${project}", targetDir)
+
+    Target(s"phony:${stage}-all") dependsOn t
     t
   }
 
-  val lockerLibJavac = stagedJavac(
-    stage = "locker", project = "library", 
-    classpath = forkjoinJar,
-    args = Seq("-XDignore.symbol.file")
+  val lockerLibJavac = stagedJavac(stage = "locker", project = "library", args = Seq("-XDignore.symbol.file"),
+    classpath = forkjoinJar
   )
 
-  val lockerLib = stagedScalac(
-    stage = "locker", project = "library", compilerClasspath = starrCompilerCp,
-    classpath = forkjoinJar ~ ant ~ lockerLibJavac,
-    sourcepath = Some(Path("src/library"))
+  val lockerLib = stagedScalac(stage = "locker", project = "library", compilerClasspath = starrCompilerCp,
+    classpath = forkjoinJar ~ ant ~ lockerLibJavac, sourcepath = Some(Path("src/library"))
   ) ~ lockerLibJavac
 
-  val lockerReflect = stagedScalac(
-    stage = "locker", project = "reflect", compilerClasspath = starrCompilerCp,
-    classpath = forkjoinJar ~ ant ~ lockerLib
-  )
+  val lockerReflect = stagedScalac(stage = "locker", project = "reflect", compilerClasspath = starrCompilerCp,
+    classpath = forkjoinJar ~ ant ~ lockerLib)
 
-  val lockerCompiler = stagedScalac(
-    stage = "locker", project = "compiler", compilerClasspath = starrCompilerCp,
+  val lockerCompiler = stagedScalac(stage = "locker", project = "compiler", compilerClasspath = starrCompilerCp,
     classpath = forkjoinJar ~ asmClasses ~ ant ~ lockerLib ~ lockerReflect
   )
 
   val lockerCompilerCp = lockerLib ~ lockerReflect ~ lockerCompiler ~ asmClasses
 
-  val quickLibJavac = stagedJavac(
-    stage = "quick", project = "library", 
-    classpath = forkjoinJar,
-    args = Seq("-XDignore.symbol.file")
+  val quickLibJavac = stagedJavac(stage = "quick", project = "library", args = Seq("-XDignore.symbol.file"),
+    classpath = forkjoinJar
   )
-  val quickLib = stagedScalac(
-    stage = "quick", project = "library", compilerClasspath = lockerCompilerCp,
-    classpath = forkjoinJar ~ ant ~ quickLibJavac,
-    sourcepath = Some(Path("src/library"))
+  val quickLib = stagedScalac(stage = "quick", project = "library", compilerClasspath = lockerCompilerCp, sourcepath = Some(Path("src/library")),
+    classpath = forkjoinJar ~ ant ~ quickLibJavac
   ) ~ quickLibJavac
 
-  val quickActorsJava = stagedJavac(
-    stage = "quick", project = "actors", 
+  val quickActorsJava = stagedJavac(stage = "quick", project = "actors", args = Seq("-XDignore.symbol.file"),
     // outputDir = Some("target/quick/library/classes"),
-    classpath = forkjoinJar,
-    args = Seq("-XDignore.symbol.file")
+    classpath = forkjoinJar
   )
-  val quickActors = stagedScalac(
-    stage = "quick", project = "actors",
+  val quickActors = stagedScalac(stage = "quick", project = "actors", compilerClasspath = lockerCompilerCp,
     // outputDir = Some("target/quick/library/classes"),
-    compilerClasspath = lockerCompilerCp,
     classpath = forkjoinJar ~ quickLib ~ quickActorsJava
   ) ~ quickActorsJava
 
-  val quickReflect = stagedScalac(
-    stage = "quick", project = "reflect", compilerClasspath = lockerCompilerCp,
+  val quickReflect = stagedScalac(stage = "quick", project = "reflect", compilerClasspath = lockerCompilerCp,
     classpath = forkjoinJar ~ ant ~ quickLib
   )
 
-  val quickCompiler = stagedScalac(
-    stage = "quick", project = "compiler", compilerClasspath = lockerCompilerCp,
+  val quickCompiler = stagedScalac(stage = "quick", project = "compiler", compilerClasspath = lockerCompilerCp,
     classpath = forkjoinJar ~ asmClasses ~ ant ~ quickLib ~ quickReflect
   )
 
-  val quickReplJava = stagedJavac(
-    stage = "quick", project = "repl", 
+  val quickReplJava = stagedJavac(stage = "quick", project = "repl",
     classpath = forkjoinJar ~ asmClasses ~ ant ~ quickLib ~ quickReflect ~ quickCompiler
   )
-  val quickRepl = stagedScalac(
-    stage = "quick", project = "repl", compilerClasspath = lockerCompilerCp,
+  val quickRepl = stagedScalac(stage = "quick", project = "repl", compilerClasspath = lockerCompilerCp,
     classpath = forkjoinJar ~ asmClasses ~ ant ~ jline ~ quickLib ~ quickReflect ~ quickCompiler ~ quickReplJava
   )
 
-  val quickScalacheck = stagedScalac(
-    stage = "quick", project = "scalacheck", compilerClasspath = lockerCompilerCp,
-    classpath =  forkjoinJar ~ asmClasses ~ quickLib ~ quickReflect ~ quickCompiler ~ quickActors,
-    args = Seq("-nowarn")
+  val quickScalacheck = stagedScalac(stage = "quick", project = "scalacheck", compilerClasspath = lockerCompilerCp, args = Seq("-nowarn"),
+    classpath =  forkjoinJar ~ asmClasses ~ quickLib ~ quickReflect ~ quickCompiler ~ quickActors
   )
 
-  val quickScalap = stagedScalac(
-    stage = "quick", project = "scalap", compilerClasspath = lockerCompilerCp,
+  val quickScalap = stagedScalac(stage = "quick", project = "scalap", compilerClasspath = lockerCompilerCp,
     classpath =  forkjoinJar ~ asmClasses ~ quickLib ~ quickReflect ~ quickCompiler
   )
 
 
-  // quick.scalacheck -> compiler* + lib-classes + scalacheck-classes
-  // quick.scalap -> 
   // quick.interactive
   // quick.swing
   // quick.plugins
